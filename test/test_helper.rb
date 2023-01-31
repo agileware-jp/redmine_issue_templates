28ff4f5ed00bb2649a9786d93f464800f5cf1c1d
yui-har <7232538+yui-har@users.noreply.github.com>
2023-01-31 09:22:03 +0900

    Add fixture global_note_templates

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -30,4 +30,5 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/',
                                          %i[issue_templates issue_template_settings
                                             global_issue_templates global_issue_templates_projects
-                                            note_templates note_visible_roles])
+                                            note_templates note_visible_roles
+                                            global_note_templates])
