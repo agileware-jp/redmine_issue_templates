2f7e24b7acb223d091aa547cbc7b5f22aa6d88b8
asakura <asakura@agileware.jp>
2023-03-30 17:23:33 +0900

    fix: use Migration version 5.2

--- db/migrate/20230330055341_change_global_note_template_projects_table_name.rb
+++ db/migrate/20230330055341_change_global_note_template_projects_table_name.rb
@@ -1,4 +1,4 @@
-class ChangeGlobalNoteTemplateProjectsTableName < ActiveRecord::Migration[6.1]
+class ChangeGlobalNoteTemplateProjectsTableName < ActiveRecord::Migration[5.2]
   def up
     rename_table :global_note_template_projects, :global_note_templates_projects
   end
